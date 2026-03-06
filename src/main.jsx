dfbb33a5960561623b30f0ececbf469761dd9aaf
Dimitar <dimitargrudov92@gmail.com>
2026-03-06 16:15:33 +0200

    add speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { inject } from '@vercel/analytics'
 import './index.css'
 import App from './App.jsx'
-
-inject();
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
